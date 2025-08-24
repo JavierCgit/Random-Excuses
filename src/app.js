a54d9f017ff890e3caed5aa545ab434e4839300e
Javier C.S. <javier.crespo.s@gmail.com>
2025-08-24 11:49:03 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'Milhouse', 'Aliens'];
+  let action = ['ate', 'peed', 'crushed', 'broke', 'hidden', 'stole', 'disintegrated'];
+  let what = ['my homework', 'my phone', 'the car', 'my last pencil', 'my router', 'my laptop charger'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying', 'when I was in the loo'];
+  
+  function getRandomIx (array){
+    return Math.floor(Math.random() * array.length);
+  }
+
+  let randomWho = who [getRandomIx(who)];
+  let randomAction = action [getRandomIx(action)];
+  let randomWhat = what [getRandomIx(what)];
+  let randomWhen = when [getRandomIx(when)];
+
+  let excuse = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + ".";
+
+  const excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = excuse;
+  
+  console.log(excuse)
 };
+
